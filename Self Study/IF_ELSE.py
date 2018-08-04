b06be7acc87642ab4dfebda27e167ef9d8d6955d
lalitp20 <42056527+lalitp20@users.noreply.github.com>
2018-08-04 15:46:04 +0530

    IF ELIF Statement

---
+++ Self Study/IF_ELSE.py
@@ -0,0 +1,15 @@
+var = 25
+if var == 40:
+   print "Condition 1 TRUE"
+   print var
+elif var == 25:
+   print "Condition 2 TRUE"
+   print var
+elif var == 65:
+   print "Condition 3 TRUE"
+   print var
+else:
+   print "Condition 4 TRUE"
+   print var
+
+print "Python Code for IF ELSE"
