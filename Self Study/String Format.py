45e961246d795f826d3dc80631618f0418e4bb25
lalitp20 <42056527+lalitp20@users.noreply.github.com>
2018-08-04 15:56:39 +0530

    String Formatting

---
+++ Self Study/String Format.py
@@ -0,0 +1,12 @@
+String1 = 'My String with one format'
+print(String1)
+ 
+String2 = "My String with Another format"
+print(String2)
+ 
+String3 = '''My String with One More format'''
+print(String3)
+ 
+Str4 = """Last More Format
+          Last More Format"""
+print(Str4)
