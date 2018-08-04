2944b2d9ce9a25f6f07cd66087660c5dcf15af4a
lalitp20 <42056527+lalitp20@users.noreply.github.com>
2018-08-04 15:46:23 +0530

    If Else Statement

---
+++ Self Study/IF_ELSE_STATEMENT.py
@@ -0,0 +1,7 @@
+Suject_Marks = int(input(" Please Enter Your Marks:  "))
+if Suject_Marks >= 35:
+    print(" Congratulations ") 
+    print(" You PASS the subject ")
+else:
+    print(" Sorry") 
+    print(" You FAIL")
