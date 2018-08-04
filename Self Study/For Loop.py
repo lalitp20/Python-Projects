fc3e294ac8e59cbca075f7007463fd8e79524a4c
lalitp20 <42056527+lalitp20@users.noreply.github.com>
2018-08-04 15:44:32 +0530

    LOOP

---
+++ Self Study/For Loop.py
@@ -0,0 +1,11 @@
+for number in range(0, 5):
+    print("Current Number: ", number)
+print("")
+
+for number in range(9, 14):
+    print("Current Number: ", number)
+print("")
+ 
+# Python For Loop with Range Example 2
+for num in range(5, 18, 2):
+    print("Current Number: ", num)
