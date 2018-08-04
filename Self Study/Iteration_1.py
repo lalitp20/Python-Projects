098abb5fd4e9b133b319433d5c43e169a62a670b
lalitp20 <42056527+lalitp20@users.noreply.github.com>
2018-08-04 15:48:35 +0530

    Add, Insert or Extend to Array

---
+++ Self Study/Iteration_1.py
@@ -0,0 +1,14 @@
+Fruits = ['Apple', 'Orange', 'Grape', 'Banana']
+ 
+# Adding items using Python append
+Fruits.append('Blackberry')
+print(Fruits)
+ 
+# inserting items using Python insert
+Fruits.insert(2, 'Kiwi')
+print(Fruits)
+ 
+# Extending List using Python extend
+Fruit_new = ['Strawberry','Cherry']
+Fruits.extend(Fruit_new) 
+print(Fruits)
