10922d77984c086deb62f2ce46058ce8526986d9
lalitp20 <42056527+lalitp20@users.noreply.github.com>
2018-08-04 15:53:09 +0530

    IF Else

---
+++ Self Study/Nested_IF_Else.py
@@ -0,0 +1,9 @@
+age = int(input(" Please Enter Your Age :  "))
+if age < 18:
+    print(" You are Child ") 
+else:
+    if age >= 18 and age <= 62:
+        print(" You are Adult or Aged ")
+    else:
+        print(" You are too Aged")
+        
