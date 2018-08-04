61df99bab2004318142c0da8d4e25bc9ac5472eb
lalitp20 <42056527+lalitp20@users.noreply.github.com>
2018-08-04 15:49:10 +0530

    Delete Statement from List

---
+++ Self Study/Iteration_3.py
@@ -0,0 +1,16 @@
+# Delete Statement example in Python List
+Fruits = ['Apple', 'Orange', 'Grape', 'Banana']
+del Fruits[1]
+print(Fruits)
+print('========\n')
+ 
+del Fruits[1]
+print(Fruits)
+print('========\n')
+ 
+del Fruits[1]
+print(Fruits)
+print('========\n')
+ 
+del Fruits[1]
+print(Fruits)
