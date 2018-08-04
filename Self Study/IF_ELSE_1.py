9cb1474390fe3dae9e366f65e38162f697080bd3
lalitp20 <42056527+lalitp20@users.noreply.github.com>
2018-08-04 15:46:13 +0530

    If Else Statement

---
+++ Self Study/IF_ELSE_1.py
@@ -0,0 +1,9 @@
+Totalmarks = int(input(" Please Enter Your Total Mark:  "))
+if Totalmarks >= 600:
+    print(" Excellent ")
+elif Totalmarks >= 500:
+    print(" Nice ")
+elif Totalmarks >= 400:
+    print(" Good ")
+else:
+    print(" OK ")
