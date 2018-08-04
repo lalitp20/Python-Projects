d589934eb0fd6cbbf9e3ed0f69dd53fe2fc6df0d
lalitp20 <42056527+lalitp20@users.noreply.github.com>
2018-08-04 15:46:34 +0530

    If Else Statement

---
+++ Self Study/IF_STATEMENT.py
@@ -0,0 +1,7 @@
+# Example for Python If Statement
+ 
+number = int(input(" Please Enter any integer Value: "))
+if number >= 1:
+    print(" You Have Entered Positive Integer ")
+else:
+    print(" You Have Entered Negative Integer ")
