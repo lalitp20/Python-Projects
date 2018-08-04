9267c6ea85d7cb8d213c19dfb4963bcf0ff2d68c
lalitp20 <42056527+lalitp20@users.noreply.github.com>
2018-08-04 15:45:25 +0530

    Function Multiplication

---
+++ Self Study/Function_2.py
@@ -0,0 +1,7 @@
+# Python Function with No Arguments, and with Return Value
+def Multiplication():
+    a = 10
+    b = 25
+    Multi = a * b
+    return Multi
+print("After Calling the Multiplication Function: ", Multiplication())
