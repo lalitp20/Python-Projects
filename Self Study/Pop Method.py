866a4a6db6d86b008c58d02622766a114165e061
lalitp20 <42056527+lalitp20@users.noreply.github.com>
2018-08-04 15:53:53 +0530

    POP Example Method

---
+++ Self Study/Pop Method.py
@@ -0,0 +1,17 @@
+# Pop Method example in Python List
+Fruits = ['Apple', 'Orange', 'Grape', 'Banana']
+ 
+Fruit = Fruits.pop(1)
+print(Fruits)
+print('Item extracted by Python Pop = ', Fruit)
+print('========\n')
+ 
+Fruit = Fruits.pop(1)
+print(Fruits)
+print('Item extracted by Python Pop = ', Fruit)
+print('========\n')
+ 
+Fruit = Fruits.pop(1)
+print(Fruits)
+print('Item extracted by Python Pop = ', Fruit)
+print('========\n')
