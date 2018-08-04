f6d236a012e4f6cae32fa3c89901d7117cf3b726
lalitp20 <42056527+lalitp20@users.noreply.github.com>
2018-08-04 15:55:48 +0530

    String Example

---
+++ Self Study/Python String.py
@@ -0,0 +1,25 @@
+x = 'Lalit Patil'
+ 
+# Slicing the String using two indexes
+a = x[2:13] 
+print("Both Indexes = ", a)
+ 
+# Slicing the String using Second indexes
+b = x[:8] 
+print("No First Index = ", b)
+ 
+# Slicing the String using First indexes
+c = x[4:] 
+print("No Second Index = ", c)
+ 
+# Slicing the String without using two indexes
+d = x[:] 
+print("No Indexes = ", d)
+ 
+# Slicing the String using Negative indexes
+e = x[-3:] 
+print("Negative First Index = ", e)
+ 
+# Slicing the String using Negative indexes
+f = x[:-2] 
+print("Negative Second Index = ", f)
