3faf3feac60238bebe424bf840bf340ede17006a
lalitp20 <42056527+lalitp20@users.noreply.github.com>
2018-08-04 15:57:07 +0530

    String Mixed Indexing Example Matrix

---
+++ Self Study/String Indexing_1.py
@@ -0,0 +1,20 @@
+x = (11, 21, 31, 41, 51, 61, 71, 81, 91)
+ 
+# Positive Indexing
+print(x[0])
+print(x[3])
+print(x[6])
+print('=======\n')
+ 
+# Negative Indexing
+print(x[-1])
+print(x[-5])
+print(x[-7])
+print('=======\n')
+ 
+# Accessing Nested Tuple Items
+Mixed_Tuple = ((1, 2, 3), [4, 5, 6], 'Lalit')
+print(Mixed_Tuple[0][0])
+print(Mixed_Tuple[1][0])
+print(Mixed_Tuple[2][0])
+print(Mixed_Tuple[2][4])
