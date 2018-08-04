45ac781e5f1c9316a2e9ebfb8033a97f44a0469b
lalitp20 <42056527+lalitp20@users.noreply.github.com>
2018-08-04 15:51:11 +0530

    Loop with String

---
+++ Self Study/Loop String.py
@@ -0,0 +1,13 @@
+Str = "Lalit Patil"
+for word in Str: # First For Loop
+    print("Letters are: ", word)
+  
+print("----This is Outside String For Loop---")
+print(" ")
+ 
+# Python For Loop - List Example
+Countries = ['ASIA', 'U S A', 'A F R I C A', 'Europe']
+for Country in Countries: # Second For Loop
+    print("Countries are: ", Country)
+    
+print("---This is Outside List For Loop")
