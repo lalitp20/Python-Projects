9ce6a92af5dc66f711ceaac01d606e0867147362
lalitp20 <42056527+lalitp20@users.noreply.github.com>
2018-08-04 15:46:55 +0530

    String Concatenation

---
+++ Self Study/Iteration Python.py
@@ -0,0 +1,21 @@
+X = 'Lalit '
+Y = 'Patil'
+ 
+# Using + Operator
+Concat = X + Y 
+Concat1 = 'Rachana ' + X
+ 
+# Placing two string Literals Together
+Concat2 = 'Markande ' ''
+ 
+#Using Parentheses
+Concat3 = ('LALIT ' 'RACHANA') 
+ 
+print(Concat)
+print(Concat1)
+print(Concat2)
+print(Concat3)
+ 
+# Using * Operator
+Z = X * 3
+print(Z)
