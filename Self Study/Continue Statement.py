c60e82efd85163485a270f3dedb09bf1baf3959c
lalitp20 <42056527+lalitp20@users.noreply.github.com>
2018-08-04 15:43:13 +0530

    Continue Statement Skip

---
+++ Self Study/Continue Statement.py
@@ -0,0 +1,6 @@
+number = int(input(" Please Enter any integer Value: "))
+for i in range (1, number):
+    if(i%2 != 0):
+        print(" Odd Numbers = {0}(Skipped By Continue)".format(i))
+        continue
+    print(" Even numbers = ", i)
