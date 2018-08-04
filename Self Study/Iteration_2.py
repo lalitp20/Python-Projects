3c46ee5d49960f5bf7ac169716856090bdbf5bcc
lalitp20 <42056527+lalitp20@users.noreply.github.com>
2018-08-04 15:48:49 +0530

    Remove Word from String

---
+++ Self Study/Iteration_2.py
@@ -0,0 +1,16 @@
+# Removing Grape Item from Fruit List
+Fruit1 = ['Apple', 'Orange', 'Grape', 'Banana']
+Fruit1.remove('Grape')
+print(Fruit1)
+print('========\n')
+ 
+# Removing Orange Item from Fruits List
+Fruit2 = ['Apple', 'Orange', 'Grape', 'Banana', 'Orange']
+Fruit2.remove('Orange')
+print(Fruit2)
+print('========\n')
+ 
+# Removing Non Existing Grape Item from Fruits1 List
+Fruit3 = ['Apple', 'Orange', 'Banana']
+Fruit3.remove('Grape')
+print(Fruit3)
