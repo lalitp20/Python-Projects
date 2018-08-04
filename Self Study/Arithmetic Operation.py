cbad51618910cff01666aa71906f51d4a0e36869
lalitp20 <42056527+lalitp20@users.noreply.github.com>
2018-08-04 15:41:18 +0530

    Arithmetic Operations

---
+++ Self Study/Arithmetic Operation.py
@@ -0,0 +1,16 @@
+a = 3.5
+b = 12
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+modulus = a % b
+exponent = a ** b
+Floor_Division = a // b
+print('Addition of two numbers {0} and {1} is {2}: '.format(a,b, addition))
+print('Subtracting Number {0} from {1} is {2}: ' .format(a,b, subtraction))
+print("Multiplication of two numbers {0} and {1} is {2}: " .format(a,b, multiplication))
+print("Division of two numbers 12 and 3 is : ", division)
+print("Modulus of two numbers 12 and 3 is : ", modulus)
+print("Exponent of two numbers 12 and 3 is : ", exponent)
+print("Floor Division of two numbers 12 and 3 is : ", Floor_Division)
