fae4f8f4dbd4e6ef88746ff9354fa21a1d23b75d
lalitp20 <42056527+lalitp20@users.noreply.github.com>
2018-08-04 15:58:07 +0530

    While IF Continue Statement

---
+++ Self Study/While_Continue Statement.py
@@ -0,0 +1,8 @@
+i = 3
+while(i <= 10):
+    if (i== 5 or i == 9):
+        print("Skipped Values =  ", i)
+        i = i + 1
+        continue
+    print(" The Value of the Variable i is:  ", i)
+    i = i + 1
