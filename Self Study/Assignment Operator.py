c45ce68e97c606257b4df7684c65e48cdaf72d33
lalitp20 <42056527+lalitp20@users.noreply.github.com>
2018-08-04 15:41:32 +0530

    Assignment Operators

---
+++ Self Study/Assignment Operator.py
@@ -0,0 +1,25 @@
+a = 7
+Total = 21
+Total += a # Using += Operator
+print("The Value of the Total after using += Operator is: ", Total)
+Total -= a # Using -= Operator
+print("The Value of the Total after using -= Operator is: ", Total)
+Total *= a # Using *= Operator
+print("The Value of the Total after using *= Operator is: ", Total)
+Total //= a # Using //= Operator
+print("The Value of the Total after using //= Operator is: ", Total)
+Total **= a # Using **= Operator
+print("The Value of the Total after using **= Operator is: ", Total)
+Total /= a # Using /= Operator
+print("The Value of the Total after using /= Operator is: ", Total)
+Total %= a # Using %= Operator
+print("The Value of the Total after using %= Operator is: ", Total)
+
+x = 9
+y = 65
+x &= y # Using &= Operator
+print("The Value of the x after using &= Operator is: ", x)
+x |= 9 # Using |= Operator
+print("The Value of the x after using |= Operator is: ", x)
+x ^= y # Using ^= Operator
+print("The Value of the x after using ^= Operator is: ", x)
