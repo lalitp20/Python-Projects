b188bc629cefd99d9cb8f089746e46eaea3efac0
lalitp20 <42056527+lalitp20@users.noreply.github.com>
2018-08-04 15:58:15 +0530

    While Statement

---
+++ Self Study/While_Statement.py
@@ -0,0 +1,6 @@
+total = 0
+number = int(input(" Please Enter any integer below 10:  "))
+while (number <= 10):
+    total = total + number
+    number = number + 1
+print(" Value of Total From the While Loop is: ", total)
