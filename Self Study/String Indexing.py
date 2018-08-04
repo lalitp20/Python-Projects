d6e1fa622a9e8d461edf3901aceb8498b0538662
lalitp20 <42056527+lalitp20@users.noreply.github.com>
2018-08-04 15:56:46 +0530

    String Indexing

---
+++ Self Study/String Indexing.py
@@ -0,0 +1,13 @@
+x = 'Lalit Patil Rachana Markande'
+ 
+# Positive Indexing
+print(x[0])
+print(x[4])
+print(x[9])
+print('=======\n')
+ 
+# Negative Indexing
+print(x[-3])
+print(x[-7])
+print(x[-16])
+print('=======\n')
