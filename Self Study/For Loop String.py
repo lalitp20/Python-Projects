f8c4df1c79367ab435692027e794d53a66fd2b55
lalitp20 <42056527+lalitp20@users.noreply.github.com>
2018-08-04 15:43:39 +0530

    Loop Statement

---
+++ Self Study/For Loop String.py
@@ -0,0 +1,11 @@
+Str = "INDIA"
+for word in range(len(Str)):
+    print("Letters at index {0} is = {1}".format(word, Str[word]))
+  
+print("----This is Outside String For Loop---")
+print(" ")
+ 
+# Python For Loop - List Example
+Countries = ['Asia', 'America', 'Europe', 'Africa']
+for Country in range(len(Countries)):
+    print("Countries at index {0} is = {1}".format(Country,Countries[Country]))
